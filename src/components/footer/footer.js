a9b9120f6909bccdddcc74f5cea03749cb9ca4c8
canmeepo <canmeepo@gmail.com>
2017-04-06 13:40:07 +0400

    Update footer.js

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,23 +1,18 @@
 var json = {
     data: [{
-        "first_name": "Betty",
-        "last_name": "Sims",
-        "email": "bsims0@studiopress.com"
-    }, {
         "first_name": "Roger",
-        "last_name": "Mendoza",
-        "email": "rmendoza1@google.pl"
+        "last_name": "Lane"
     }, {
-        "first_name": "Albert",
-        "last_name": "West",
-        "email": "awest2@cornell.edu"
+        "first_name": "Roger1",
+        "last_name": "Lane1"
     }, {
-        "first_name": "Bobby",
-        "last_name": "Lane",
-        "email": "blane3@ameblo.jp"
+        "first_name": "Roger2",
+        "last_name": "Lane2"
     }, {
-        "first_name": "Julie",
-        "last_name": "Wheeler",
-        "email": "jwheeler4@google.ru"
+        "first_name": "Bobby3",
+        "last_name": "Lane3"
+    }, {
+        "first_name": "Roger4",
+        "last_name": "Lane4"
     }]
 };
